ad22a3ed6609deb557b719c06ac289599da9536f
Andy-Ugarte <andy.ugarte.schirrmeister@gmail.com>
2023-11-29 19:11:54 -0700

    verification email

--- TMServer/models/email.model.js
+++ TMServer/models/email.model.js
@@ -18,3 +18,11 @@ const EmailSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Email", EmailSchema);
+
+
+// from: '"Fred Foo 👻" <foo@example.com>', // sender address
+// to: "bar@example.com, baz@example.com", // list of receivers
+// subject: "Hello ✔", // Subject line
+// text: "Hello world?", // plain text body
+// html: "<b>Hello world?</b>", // html body
+// });
