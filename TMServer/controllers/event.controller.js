17ee987c805a78810f70cb23e8c0937009ad96ed
Andy-Ugarte <andy.ugarte.schirrmeister@gmail.com>
2023-12-06 21:22:13 -0700

    register

--- TMServer/controllers/event.controller.js
+++ TMServer/controllers/event.controller.js
@@ -3,11 +3,16 @@ const User = require("../models/users.model");
 const Event = require("../models/event.model");
 const validateSession = require("../middleware/validateSession");
 
+
+
+
+
 function errorResponse(res, err) {
   res.status(500).json({ ERROR: err.message });
 }
 // Added validate session
 router.post("/event", validateSession, async (req, res) => {
+  console.log('hello/event')
   try {
 
     const ownerId = req.user._id;
@@ -16,11 +21,12 @@ router.post("/event", validateSession, async (req, res) => {
       name: req.body.name,
       date: req.body.date,
       description: req.body.description,
-     // owner: ownerId,
+      owner: ownerId,
     };
     
     const event = new Event(eventCard);
     const newEvent = await event.save();
+    console.log(newEvent);
     res.status(200).json({
       message: "New Event Created!",
       event: newEvent,
@@ -36,10 +42,19 @@ router.patch ("/event/register/:id", validateSession, async (req, res) => {
   try {
     
     const user = req.user._id;
+    
+    //attendeeArray.push = myEvent.attendee
+
 
     const event = req.params.id
-    const register = await Event.findOneAndUpdate({_id:event})
-    console.log('Register',register);
+    const myEvent = await Event.findById({_id:event})
+
+    attendeeArray = myEvent.attendee.push(user) //save- update event, 
+
+    myEvent.save()
+
+    console.log('myEvent',myEvent);
+    console.log(myEvent.attendee) 
     
   } catch (err) {
     errorResponse(res,err);
@@ -49,3 +64,4 @@ router.patch ("/event/register/:id", validateSession, async (req, res) => {
   
 
 module.exports = router;
+//res . status. res.json 
